408a602b0ec1fc20b237305cc34dc863faaa1392
tkuiclabwhite <tkuiclabwhite@gmail.com>
2026-06-04 15:06:26 +0800

    Update (src/strategy/strategy/sp): 2026-06-04 15:06

--- src/strategy/strategy/sp/sp.py
+++ src/strategy/strategy/sp/sp.py
@@ -10,19 +10,19 @@ import time
 # 走路速度 / 轉向相關參數
 # =========================
 FORWARD_START_SPEED = 2000     # 起步前進速度 2000
-BACK_START_SPEED = -1000       # 起步後退速度（負值代表反方向) 
-FORWARD_MAX_SPEED = 8000       # 前進速度上限
-FORWARD_MIN_SPEED = 1000       # 前進減速階段的下限（這裡設成 2000 => 等於不會真的降到更慢）
-BACK_MAX_SPEED = -8000         # 後退速度上限（越接近 0 越慢；-2000 是最快後退）
+BACK_START_SPEED = -100       # 起步後退速度（負值代表反方向) 
+FORWARD_MAX_SPEED = 6500       # 前進速度上限
+FORWARD_MIN_SPEED = 2000       # 前進減速階段的下限（這裡設成 2000 => 等於不會真的降到更慢）
+BACK_MAX_SPEED = -6500         # 後退速度上限（越接近 0 越慢；-2000 是最快後退）
 
 # 每圈更新速度的變化量
 FORWARD_SPEED_ADD = 100        # 前進加速量
 FORWARD_SPEED_SUB = -100       # 減速量（負值代表速度往小變）
-BACK_SPEED_ADD = -100          # 後退加速量（更負 => 更快後退）
+BACK_SPEED_ADD = -50          # 後退加速量（更負 => 更快後退）
 
 # theta(轉向)的基準偏移
-FORWARD_ORIGIN_THETA = -1   # 前進的基準修正
-BACK_ORIGIN_THETA = -1   # 後退的基準修正（通常後退要稍微修方向）
+FORWARD_ORIGIN_THETA = 0   # 前進的基準修正
+BACK_ORIGIN_THETA = 0   # 後退的基準修正（通常後退要稍微修方向）
 
 # =========================
 # 頭部馬達上下限
