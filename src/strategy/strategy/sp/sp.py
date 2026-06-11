80132c8461f98651fed47707bf0c52b41d882b4d
tkuiclabwhite <tkuiclabwhite@gmail.com>
2026-06-11 21:11:02 +0800

    Update (src/strategy/strategy/sp): 2026-06-11 21:11

--- src/strategy/strategy/sp/sp.py
+++ src/strategy/strategy/sp/sp.py
@@ -11,17 +11,17 @@ import time
 # =========================
 FORWARD_START_SPEED = 2000     # 起步前進速度 2000
 BACK_START_SPEED = -1000       # 起步後退速度（負值代表反方向) 
-FORWARD_MAX_SPEED = 6500       # 前進速度上限
-FORWARD_MIN_SPEED = 2000       # 前進減速階段的下限（這裡設成 2000 => 等於不會真的降到更慢）
-BACK_MAX_SPEED = -6500         # 後退速度上限（越接近 0 越慢；-2000 是最快後退）
+FORWARD_MAX_SPEED = 8000       # 前進速度上限
+FORWARD_MIN_SPEED = 1500       # 前進減速階段的下限（這裡設成 2000 => 等於不會真的降到更慢）
+BACK_MAX_SPEED = -6000         # 後退速度上限（越接近 0 越慢；-2000 是最快後退）
 
 # 每圈更新速度的變化量
 FORWARD_SPEED_ADD = 100        # 前進加速量
-FORWARD_SPEED_SUB = -100       # 減速量（負值代表速度往小變）
-BACK_SPEED_ADD = -100          # 後退加速量（更負 => 更快後退）
+FORWARD_SPEED_SUB = -50       # 減速量（負值代表速度往小變）
+BACK_SPEED_ADD = -50          # 後退加速量（更負 => 更快後退）
 
 # theta(轉向)的基準偏移
-FORWARD_ORIGIN_THETA = 0   # 前進的基準修正
+FORWARD_ORIGIN_THETA = 0   # 前進的基準修正 1/0/-1
 BACK_ORIGIN_THETA = 0   # 後退的基準修正（通常後退要稍微修方向）
 
 # =========================
@@ -75,9 +75,9 @@ class SP():
         if not self.ball_found:
            return 'Forward'
         # note: 3200/5800 這兩個門檻非常吃解析度、鏡頭FOV、球在畫面上的大小 換相機/換高度 這裡通常要重調
-        if 4000 >= self.sp_ball.size >= 1000:     # 到球前減速
+        if 6000 >= self.sp_ball.size >= 1000:     # 到球前減速
             return 'Decelerating'
-        elif self.sp_ball.size > 4000:            # 準備後退（代表球已很靠近）
+        elif self.sp_ball.size > 6000:            # 準備後退（代表球已很靠近）
             return 'Backward'
         return 'Forward'
 
